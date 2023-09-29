619217ed468409e83a4f010a8706d308988819b7
sevketugurel <ugurelsevket@gmail.com>
2023-09-29 23:11:01 +0300

    Sağ sidebara Headlessden Popover ve Disclosure yardımıyla more eklendi.

--- src/layouts/main/sidebar/menu/index.jsx
+++ src/layouts/main/sidebar/menu/index.jsx
@@ -1,7 +1,8 @@
 import { NavLink } from "react-router-dom"
 import classNames from "classnames" // koşullar ekleyebilmemiz için
 import { mainMenu } from "~/utils/consts"
-import Button from "~/components/button"
+import More from "./more"
+import New from "./new"
 
 export default function Menu() {
   return (
@@ -26,24 +27,8 @@ export default function Menu() {
         </NavLink>
       ))}
 
-      {/* Daha fazla yazan kısımı buton olarak tanımlıyoruz */}
-      <button className="py-[3px] block group">
-          <div className="p-3 rounded-full transition-colors inline-flex items-center gap-5 group-hover:bg-[#eff3f41a]">
-            <div className="w-[26.25px] h-[26.25px] relative">
-              <svg viewBox="0 0 24 24" width={26.25} height={26.25}><path
-                fill="#e7e9ea"
-                d="M3.75 12c0-4.56 3.69-8.25 8.25-8.25s8.25 3.69 8.25 8.25-3.69 8.25-8.25 8.25S3.75 16.56 3.75 12zM12 1.75C6.34 1.75 1.75 6.34 1.75 12S6.34 22.25 12 22.25 22.25 17.66 22.25 12 17.66 1.75 12 1.75zm-4.75 11.5c.69 0 1.25-.56 1.25-1.25s-.56-1.25-1.25-1.25S6 11.31 6 12s.56 1.25 1.25 1.25zm9.5 0c.69 0 1.25-.56 1.25-1.25s-.56-1.25-1.25-1.25-1.25.56-1.25 1.25.56 1.25 1.25 1.25zM13.25 12c0 .69-.56 1.25-1.25 1.25s-1.25-.56-1.25-1.25.56-1.25 1.25-1.25 1.25.56 1.25 1.25z" /></svg>
-            </div>
-
-            <div className="pr-4 text-xl">
-              Daha fazla
-            </div>
-          </div>
-      </button>
-
-      <div className="py-4 w-[90%]">
-        <Button size="large">Gönder</Button>
-      </div>
+      <More />
+      <New />
     </nav>
 
 
